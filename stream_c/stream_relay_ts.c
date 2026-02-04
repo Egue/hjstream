fe9818e84a3eb10354b937e264752624e32698ce
Edwin Egue <ing.fabian23@gmail.com>
2026-02-03 21:27:13 -0500

    correcicon de fp0

--- stream_c/stream_relay_ts.c
+++ stream_c/stream_relay_ts.c
@@ -12,23 +12,20 @@
 
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
+#include <libavcodec/bsf.h>
 #include <libavutil/time.h>
 #include <libavutil/opt.h>
 
 #define MAX_STREAMS 100
-#define BUFFER_SIZE 188 * 7  // 7 paquetes TS
-#define UDP_PACKET_SIZE 1316
 #define STATS_INTERVAL 10
 
 typedef struct {
-    char name[256];
     unsigned long packets_sent;
     unsigned long bytes_sent;
     unsigned long errors;
     time_t start_time;
     int active;
-    double bitrate;
-    int reconnect_count;
+    int reconnects;
 } StreamStats;
 
 typedef struct {
@@ -37,7 +34,6 @@ typedef struct {
     char output_ip[64];
     int output_port;
     int running;
-    int remux_to_ts;  // 1 para forzar remux a MPEGTS
     pthread_t thread;
     StreamStats stats;
 } StreamConfig;
@@ -47,31 +43,24 @@ static int stream_count = 0;
 static int program_running = 1;
 static pthread_mutex_t stats_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+/* ================= STATS ================= */
+
 void print_stats() {
     pthread_mutex_lock(&stats_mutex);
-    
-    printf("\n========== ESTADÍSTICAS DE STREAMS ==========\n");
-    time_t now = time(NULL);
-    printf("Tiempo: %s", ctime(&now));
-    
+    printf("\n========= ESTADÍSTICAS =========\n");
     for (int i = 0; i < stream_count; i++) {
-        if (streams[i].stats.active) {
-            time_t elapsed = time(NULL) - streams[i].stats.start_time;
-            double mbps = elapsed > 0 ? (streams[i].stats.bytes_sent * 8.0) / (elapsed * 1000000.0) : 0;
-            
-            printf("\n[Stream %d] %s\n", i, streams[i].stats.name);
-            printf("  Input:  %s\n", streams[i].input_url);
-            printf("  Output: %s:%d (UDP)\n", streams[i].output_ip, streams[i].output_port);
-            printf("  Paquetes: %lu | Bytes: %lu MB | Errores: %lu\n", 
-                   streams[i].stats.packets_sent, 
-                   streams[i].stats.bytes_sent / (1024 * 1024),
-                   streams[i].stats.errors);
-            printf("  Bitrate: %.2f Mbps | Uptime: %ld seg | Reconexiones: %d\n", 
-                   mbps, elapsed, streams[i].stats.reconnect_count);
-        }
+        if (!streams[i].stats.active) continue;
+        time_t up = time(NULL) - streams[i].stats.start_time;
+        double mbps = (up > 0) ?
+            (streams[i].stats.bytes_sent * 8.0) / (up * 1000000.0) : 0;
+
+        printf("[Stream %d] %.2f Mbps | Pkts: %lu | Err: %lu | Reconn: %d\n",
+            i, mbps,
+            streams[i].stats.packets_sent,
+            streams[i].stats.errors,
+            streams[i].stats.reconnects);
     }
-    
-    printf("============================================\n\n");
+    printf("===============================\n");
     pthread_mutex_unlock(&stats_mutex);
 }
 
@@ -83,337 +72,178 @@ void* stats_thread(void* arg) {
     return NULL;
 }
 
-// Versión mejorada con remuxing a MPEGTS
-void* stream_relay_remux_thread(void* arg) {
-    StreamConfig* config = (StreamConfig*)arg;
-    AVFormatContext* input_ctx = NULL;
-    AVFormatContext* output_ctx = NULL;
-    AVPacket* packet = NULL;
-    AVDictionary* opts = NULL;
-    int ret;
-    int sockfd;
-    struct sockaddr_in dest_addr;
-    char output_url[256];
-    int64_t last_pts[128] = {0}; // Para tracking de PTS por stream
-    
-    pthread_mutex_lock(&stats_mutex);
-    config->stats.active = 1;
-    config->stats.start_time = time(NULL);
-    snprintf(config->stats.name, sizeof(config->stats.name), "Stream_%d", config->id);
-    pthread_mutex_unlock(&stats_mutex);
-    
-    printf("[Stream %d] Iniciando con remux a MPEGTS: %s -> %s:%d\n", 
-           config->id, config->input_url, config->output_ip, config->output_port);
-    
+/* ================= STREAM THREAD ================= */
+
+void* stream_thread(void* arg) {
+    StreamConfig* cfg = (StreamConfig*)arg;
+    AVFormatContext *in = NULL, *out = NULL;
+    AVPacket *pkt = NULL;
+    AVBSFContext *bsf = NULL;
+    int video_index = -1;
+    int got_keyframe = 0;
+
 reconnect:
-    if (!config->running || !program_running) goto cleanup;
-    
-    // Crear socket UDP
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        fprintf(stderr, "[Stream %d] Error creando socket: %s\n", config->id, strerror(errno));
-        pthread_mutex_lock(&stats_mutex);
-        config->stats.errors++;
-        pthread_mutex_unlock(&stats_mutex);
-        sleep(5);
-        goto reconnect;
-    }
-    
-    // Configurar socket
-    int sendbuf = 4 * 1024 * 1024; // 4MB buffer
-    setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &sendbuf, sizeof(sendbuf));
-    
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(config->output_port);
-    inet_pton(AF_INET, config->output_ip, &dest_addr.sin_addr);
-    
-    // Configurar opciones de input
-    av_dict_set(&opts, "timeout", "10000000", 0);
-    av_dict_set(&opts, "buffer_size", "2097152", 0);
-    av_dict_set(&opts, "rtbufsize", "200M", 0);
-    av_dict_set(&opts, "analyzeduration", "3000000", 0);
-    av_dict_set(&opts, "probesize", "2000000", 0);
-    av_dict_set(&opts, "fflags", "nobuffer", 0);
-    av_dict_set(&opts, "flags", "low_delay", 0);
-    
-    // Abrir input
-    ret = avformat_open_input(&input_ctx, config->input_url, NULL, &opts);
-    av_dict_free(&opts);
-    
-    if (ret < 0) {
-        char errbuf[128];
-        av_strerror(ret, errbuf, sizeof(errbuf));
-        fprintf(stderr, "[Stream %d] Error abriendo input: %s\n", config->id, errbuf);
-        close(sockfd);
-        pthread_mutex_lock(&stats_mutex);
-        config->stats.errors++;
-        config->stats.reconnect_count++;
-        pthread_mutex_unlock(&stats_mutex);
-        sleep(5);
-        goto reconnect;
-    }
-    
-    ret = avformat_find_stream_info(input_ctx, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "[Stream %d] Error obteniendo stream info\n", config->id);
-        avformat_close_input(&input_ctx);
-        close(sockfd);
-        pthread_mutex_lock(&stats_mutex);
-        config->stats.errors++;
-        pthread_mutex_unlock(&stats_mutex);
-        sleep(5);
-        goto reconnect;
-    }
-    
-    printf("[Stream %d] Input abierto: %s (%d streams)\n", 
-           config->id, input_ctx->iformat->name, input_ctx->nb_streams);
-    
-    // Crear output context para UDP MPEGTS
-    snprintf(output_url, sizeof(output_url), "udp://%s:%d?pkt_size=1316&buffer_size=65535",
-             config->output_ip, config->output_port);
-    
-    ret = avformat_alloc_output_context2(&output_ctx, NULL, "mpegts", output_url);
-    if (ret < 0 || !output_ctx) {
-        fprintf(stderr, "[Stream %d] Error creando output context\n", config->id);
-        avformat_close_input(&input_ctx);
-        close(sockfd);
-        pthread_mutex_lock(&stats_mutex);
-        config->stats.errors++;
-        pthread_mutex_unlock(&stats_mutex);
-        sleep(5);
-        goto reconnect;
-    }
-    
-    // Copiar streams del input al output
-    for (unsigned int i = 0; i < input_ctx->nb_streams; i++) {
-        AVStream* in_stream = input_ctx->streams[i];
-        AVStream* out_stream = avformat_new_stream(output_ctx, NULL);
-        
-        if (!out_stream) {
-            fprintf(stderr, "[Stream %d] Error creando output stream\n", config->id);
-            goto cleanup_contexts;
-        }
-        
-        ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
-        if (ret < 0) {
-            fprintf(stderr, "[Stream %d] Error copiando parámetros de codec\n", config->id);
-            goto cleanup_contexts;
-        }
-        
-        out_stream->codecpar->codec_tag = 0;
-        out_stream->time_base = in_stream->time_base;
-    }
-    
-    // Configurar opciones de MPEGTS output
-    AVDictionary* output_opts = NULL;
-    av_dict_set(&output_opts, "mpegts_flags", "initial_discontinuity", 0);
-    av_dict_set_int(output_ctx->priv_data, "muxrate", 0, 0);
-    
-    // Abrir output
-    if (!(output_ctx->oformat->flags & AVFMT_NOFILE)) {
-        ret = avio_open2(&output_ctx->pb, output_url, AVIO_FLAG_WRITE, NULL, &output_opts);
-        av_dict_free(&output_opts);
-        
-        if (ret < 0) {
-            char errbuf[128];
-            av_strerror(ret, errbuf, sizeof(errbuf));
-            fprintf(stderr, "[Stream %d] Error abriendo output: %s\n", config->id, errbuf);
-            goto cleanup_contexts;
-        }
-    }
-    
-    // Escribir header
-    ret = avformat_write_header(output_ctx, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "[Stream %d] Error escribiendo header\n", config->id);
-        goto cleanup_contexts;
-    }
-    
-    printf("[Stream %d] Output MPEGTS configurado correctamente\n", config->id);
-    
-    // Allocar packet
-    packet = av_packet_alloc();
-    if (!packet) {
-        fprintf(stderr, "[Stream %d] Error allocando packet\n", config->id);
-        goto cleanup_contexts;
-    }
-    
-    // Loop principal
-    while (config->running && program_running) {
-        ret = av_read_frame(input_ctx, packet);
-        
-        if (ret < 0) {
-            if (ret == AVERROR_EOF) {
-                printf("[Stream %d] Fin del stream, reconectando...\n", config->id);
-                av_packet_unref(packet);
-                break;
-            } else if (ret == AVERROR(EAGAIN)) {
-                usleep(1000);
-                continue;
-            } else {
-                char errbuf[128];
-                av_strerror(ret, errbuf, sizeof(errbuf));
-                fprintf(stderr, "[Stream %d] Error leyendo: %s\n", config->id, errbuf);
-                pthread_mutex_lock(&stats_mutex);
-                config->stats.errors++;
-                pthread_mutex_unlock(&stats_mutex);
-                av_packet_unref(packet);
-                break;
-            }
-        }
-        
-        // Rescalar timestamps
-        AVStream* in_stream = input_ctx->streams[packet->stream_index];
-        AVStream* out_stream = output_ctx->streams[packet->stream_index];
-        
-        packet->pts = av_rescale_q_rnd(packet->pts, in_stream->time_base, 
-                                       out_stream->time_base, 
-                                       AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX);
-        packet->dts = av_rescale_q_rnd(packet->dts, in_stream->time_base, 
-                                       out_stream->time_base, 
-                                       AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX);
-        packet->duration = av_rescale_q(packet->duration, in_stream->time_base, 
-                                        out_stream->time_base);
-        packet->pos = -1;
-        
-        // Escribir packet
-        ret = av_interleaved_write_frame(output_ctx, packet);
-        
-        if (ret < 0) {
-            pthread_mutex_lock(&stats_mutex);
-            config->stats.errors++;
-            pthread_mutex_unlock(&stats_mutex);
-        } else {
-            pthread_mutex_lock(&stats_mutex);
-            config->stats.packets_sent++;
-            config->stats.bytes_sent += packet->size;
-            pthread_mutex_unlock(&stats_mutex);
-        }
-        
-        av_packet_unref(packet);
-    }
-    
-    // Escribir trailer
-    if (output_ctx) {
-        av_write_trailer(output_ctx);
-    }
-    
-cleanup_contexts:
-    if (packet) av_packet_free(&packet);
-    if (output_ctx) {
-        if (output_ctx->pb) avio_closep(&output_ctx->pb);
-        avformat_free_context(output_ctx);
-    }
-    if (input_ctx) avformat_close_input(&input_ctx);
-    close(sockfd);
-    
-    if (config->running && program_running) {
-        printf("[Stream %d] Reconectando en 5 segundos...\n", config->id);
-        sleep(5);
-        goto reconnect;
-    }
-    
-cleanup:
+    if (!cfg->running || !program_running) goto end;
+
     pthread_mutex_lock(&stats_mutex);
-    config->stats.active = 0;
+    cfg->stats.active = 1;
+    cfg->stats.start_time = time(NULL);
+    cfg->stats.reconnects++;
     pthread_mutex_unlock(&stats_mutex);
-    
-    printf("[Stream %d] Thread finalizado\n", config->id);
+
+    AVDictionary *opts = NULL;
+    av_dict_set(&opts, "latency", "200000", 0);
+    av_dict_set(&opts, "rcvbuf", "20000000", 0);
+    av_dict_set(&opts, "peerlatency", "200000", 0);
+
+    if (avformat_open_input(&in, cfg->input_url, NULL, &opts) < 0) {
+        av_dict_free(&opts);
+        sleep(3);
+        goto reconnect;
+    }
+    av_dict_free(&opts);
+
+    if (avformat_find_stream_info(in, NULL) < 0)
+        goto cleanup;
+
+    avformat_alloc_output_context2(&out, NULL, "mpegts", NULL);
+    if (!out) goto cleanup;
+
+    for (unsigned i = 0; i < in->nb_streams; i++) {
+        AVStream *is = in->streams[i];
+        AVStream *os = avformat_new_stream(out, NULL);
+        avcodec_parameters_copy(os->codecpar, is->codecpar);
+        os->time_base = is->time_base;
+
+        if (is->codecpar->codec_type == AVMEDIA_TYPE_VIDEO &&
+            is->codecpar->codec_id == AV_CODEC_ID_H264) {
+            video_index = i;
+        }
+    }
+
+    if (avio_open(&out->pb,
+        av_asprintf("udp://%s:%d?pkt_size=1316",
+            cfg->output_ip, cfg->output_port),
+        AVIO_FLAG_WRITE) < 0)
+        goto cleanup;
+
+    if (avformat_write_header(out, NULL) < 0)
+        goto cleanup;
+
+    /* ===== Bitstream filter H264 ===== */
+    if (video_index >= 0) {
+        const AVBitStreamFilter *filter =
+            av_bsf_get_by_name("h264_mp4toannexb");
+        av_bsf_alloc(filter, &bsf);
+        avcodec_parameters_copy(
+            bsf->par_in, in->streams[video_index]->codecpar);
+        bsf->time_base_in = in->streams[video_index]->time_base;
+        av_bsf_init(bsf);
+    }
+
+    pkt = av_packet_alloc();
+
+    while (program_running && cfg->running) {
+        if (av_read_frame(in, pkt) < 0)
+            break;
+
+        if (pkt->stream_index == video_index) {
+            if (!got_keyframe) {
+                if (!(pkt->flags & AV_PKT_FLAG_KEY)) {
+                    av_packet_unref(pkt);
+                    continue;
+                }
+                got_keyframe = 1;
+            }
+
+            av_bsf_send_packet(bsf, pkt);
+            while (av_bsf_receive_packet(bsf, pkt) == 0) {
+                av_interleaved_write_frame(out, pkt);
+                pthread_mutex_lock(&stats_mutex);
+                cfg->stats.packets_sent++;
+                cfg->stats.bytes_sent += pkt->size;
+                pthread_mutex_unlock(&stats_mutex);
+                av_packet_unref(pkt);
+            }
+            continue;
+        }
+
+        av_interleaved_write_frame(out, pkt);
+        av_packet_unref(pkt);
+    }
+
+cleanup:
+    if (pkt) av_packet_free(&pkt);
+    if (bsf) av_bsf_free(&bsf);
+    if (out) {
+        av_write_trailer(out);
+        if (out->pb) avio_closep(&out->pb);
+        avformat_free_context(out);
+    }
+    if (in) avformat_close_input(&in);
+
+    sleep(3);
+    goto reconnect;
+
+end:
+    pthread_mutex_lock(&stats_mutex);
+    cfg->stats.active = 0;
+    pthread_mutex_unlock(&stats_mutex);
     return NULL;
 }
 
-int load_config(const char* filename) {
-    FILE* fp = fopen(filename, "r");
-    if (!fp) {
-        fprintf(stderr, "Error abriendo configuración: %s\n", filename);
-        return -1;
+/* ================= MAIN ================= */
+
+void signal_handler(int s) {
+    program_running = 0;
+    for (int i = 0; i < stream_count; i++)
+        streams[i].running = 0;
+}
+
+int load_config(const char *f) {
+    FILE *fp = fopen(f, "r");
+    if (!fp) return -1;
+
+    char in[512], ip[64];
+    int port;
+    while (fscanf(fp, "%s %s %d", in, ip, &port) == 3) {
+        streams[stream_count].id = stream_count;
+        strcpy(streams[stream_count].input_url, in);
+        strcpy(streams[stream_count].output_ip, ip);
+        streams[stream_count].output_port = port;
+        streams[stream_count].running = 1;
+        memset(&streams[stream_count].stats, 0, sizeof(StreamStats));
+        stream_count++;
     }
-    
-    char line[1024];
-    stream_count = 0;
-    
-    while (fgets(line, sizeof(line), fp) && stream_count < MAX_STREAMS) {
-        if (line[0] == '#' || line[0] == '\n') continue;
-        
-        char input[512], output_ip[64];
-        int output_port;
-        
-        if (sscanf(line, "%s %s %d", input, output_ip, &output_port) == 3) {
-            streams[stream_count].id = stream_count;
-            strncpy(streams[stream_count].input_url, input, sizeof(streams[stream_count].input_url) - 1);
-            strncpy(streams[stream_count].output_ip, output_ip, sizeof(streams[stream_count].output_ip) - 1);
-            streams[stream_count].output_port = output_port;
-            streams[stream_count].running = 1;
-            streams[stream_count].remux_to_ts = 1; // Siempre remux a TS
-            
-            memset(&streams[stream_count].stats, 0, sizeof(StreamStats));
-            
-            stream_count++;
-        }
-    }
-    
     fclose(fp);
-    printf("Configuración cargada: %d streams\n", stream_count);
     return stream_count;
 }
 
-void signal_handler(int sig) {
-    printf("\nSeñal recibida (%d). Deteniendo...\n", sig);
-    program_running = 0;
-    
-    for (int i = 0; i < stream_count; i++) {
-        streams[i].running = 0;
-    }
-}
-
-int main(int argc, char* argv[]) {
-    pthread_t stats_tid;
-    
-    printf("===========================================\n");
-    printf("  Multi-hjstreamc MPEGTS para CATV-2\n");
-    printf("  Optimizado - Bajo CPU - Auto-reconexión\n");
-    printf("===========================================\n\n");
-    
+int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Uso: %s <config.txt>\n", argv[0]);
+        printf("Uso: %s config.txt\n", argv[0]);
         return 1;
     }
-    
+
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
-    signal(SIGPIPE, SIG_IGN);
-    
+
     av_log_set_level(AV_LOG_ERROR);
-    
-    if (load_config(argv[1]) <= 0) {
-        fprintf(stderr, "Error cargando configuración\n");
+
+    if (load_config(argv[1]) <= 0)
         return 1;
-    }
-    
-    pthread_create(&stats_tid, NULL, stats_thread, NULL);
-    
-    for (int i = 0; i < stream_count; i++) {
-        if (pthread_create(&streams[i].thread, NULL, stream_relay_remux_thread, &streams[i]) != 0) {
-            fprintf(stderr, "Error creando thread %d\n", i);
-            streams[i].running = 0;
-        } else {
-            usleep(200000); // 200ms entre streams
-        }
-    }
-    
-    printf("\n%d streams activos. Ctrl+C para detener.\n\n", stream_count);
-    
-    for (int i = 0; i < stream_count; i++) {
-        if (streams[i].thread) {
-            pthread_join(streams[i].thread, NULL);
-        }
-    }
-    
+
+    pthread_t st;
+    pthread_create(&st, NULL, stats_thread, NULL);
+
+    for (int i = 0; i < stream_count; i++)
+        pthread_create(&streams[i].thread, NULL,
+                       stream_thread, &streams[i]);
+
+    for (int i = 0; i < stream_count; i++)
+        pthread_join(streams[i].thread, NULL);
+
     program_running = 0;
-    pthread_join(stats_tid, NULL);
-    
-    print_stats();
-    printf("\nPrograma finalizado.\n");
+    pthread_join(st, NULL);
     return 0;
 }
